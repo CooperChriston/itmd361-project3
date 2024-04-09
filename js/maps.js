4aa6365ed010e59e496a4ac8831acb2f0b14b8d5
CooperChriston <cchriston@hawk.iit.edu>
2024-04-09 15:05:26 -0500

    Changes to Map

--- js/maps.js
+++ js/maps.js
@@ -1,15 +1,18 @@
+var map;
+var marker;
+const seoul = {lat: 37.563128240087636, lng: 126.98991986638902};
+const tokyo = {lat: 35.696918456946136, lng: 139.76791730924373}
+
 function initMap() {
 
-    const seoul = {lat: 37.563128240087636, lng: 126.98991986638902};
-
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         center: seoul,
         zoom: 10
     });
 
-    // Feature 1: Marker
+    // Feature 1: Marker and Marker location changing
     // Place a heart marker on Seoul
-    var marker = new google.maps.Marker({
+    marker = new google.maps.Marker({
         map: map,
         position: seoul,
         animation: google.maps.Animation.BOUNCE,
@@ -19,9 +22,38 @@ function initMap() {
         }
     });
 
-    // Feature 2: Transit Layer
-    var transit = new google.maps.TransitLayer();
-    transit.setMap(map);
+    function changeLocation() {
+
+        if (marker.getPosition() == seoul) {
+
+            marker.setPosition(tokyo);
+
+        } else {
+
+            marker.setPosition(seoul);
+        }
+    }
+
+    // // Feature 2: Transit Layer
+    // const transit = new google.maps.TransitLayer();
+    // transit.setMap(map);
+
+    // const traffic = new google.maps.TrafficLayer();
+    // traffic.setMap(map);
+
+    // const bike = new google.maps.BicyclingLayer();
+    // bike.setMap(map);
+
+    // Feature 2: Info window of specific locations
+    const infowindow = new google.maps.InfoWindow({
+        content: "Seoul is one of the many places I would like to visit later on in life.",
+        ariaLabel: "Seoul",
+      });
+
+    marker.addListener("click", () => {
+        infowindow.open({
+          anchor: marker,
+          map,
+        });
+    });
 }
-  
-//google.maps.event.addDomListener(window, 'load', initMap);
