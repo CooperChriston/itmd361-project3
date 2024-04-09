823e2a8336791c166f600a744ec334327496cb30
CooperChriston <77741675+CooperChriston@users.noreply.github.com>
2024-04-09 13:21:52 -0500

    Added Marker
    Added marker feature the map, and testing traffic layer

--- js/maps.js
+++ js/maps.js
@@ -1,8 +1,27 @@
-function initMap(){
+function initMap() {
+
+    const seoul = {lat: 37.563128240087636, lng: 126.98991986638902};
+
     var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
+        center: seoul,
+        zoom: 10
     });
+
+    // Feature 1: Marker
+    // Place a heart marker on Seoul
+    var marker = new google.maps.Marker({
+        map: map,
+        position: seoul,
+        animation: google.maps.Animation.BOUNCE,
+        icon: {
+            url: 'images/heartMarker.webp',
+            scaledSize: new google.maps.Size(50, 50)
+        }
+    });
+
+    // Feature 2: Transit Layer
+    var transit = new google.maps.TransitLayer();
+    transit.setMap(map);
 }
   
 google.maps.event.addDomListener(window, 'load', initMap);
